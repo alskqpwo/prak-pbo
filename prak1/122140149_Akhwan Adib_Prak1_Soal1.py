f4890f096c49fcccc25dfefc4018967c53118b8a
alskqpwo <49271772+alskqpwo@users.noreply.github.com>
2024-03-02 21:02:00 +0700

    Update 122140149_Akhwan Adib_Prak1_Soal1.py

--- prak1/122140149_Akhwan Adib_Prak1_Soal1.py
+++ prak1/122140149_Akhwan Adib_Prak1_Soal1.py
@@ -1,1 +1,17 @@
-p
+num1 = int(input("batas bawah: "))
+num2 = int(input("batas atas: "))
+count = 0
+
+if num1 < 0 or num2 < 0:
+    print("Batas bawah dan atas tidak boleh di bawah Nol")
+elif num1 % 2 == 1:
+    for i in range(num1, num2, 2):
+        print(i)
+        count += i
+else:
+    num1 += 1
+    for i in range(num1, num2, 2):
+        print(i)
+        count += i
+        
+print("total :", count)
