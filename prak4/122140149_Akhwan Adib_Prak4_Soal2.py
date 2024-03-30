c91b68f476f3aa6e19acdf463f19548309883867
alskqpwo <49271772+alskqpwo@users.noreply.github.com>
2024-03-30 12:01:09 +0700

    Create 122140149_Akhwan Adib_Prak4_Soal2.py

---
+++ prak4/122140149_Akhwan Adib_Prak4_Soal2.py
@@ -0,0 +1,23 @@
+#pi = 3.14159265358979323846
+from math import pi
+
+class Persegi:
+    def __init__(self, sisi):
+        self.sisi = sisi
+
+    def hitungLuas(self):
+        return self.sisi ** 2
+
+
+class Lingkaran:
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
+
+    def hitungLuas(self):
+        return pi * self.jari_jari ** 2
+
+persegi = Persegi(5)
+lingkaran = Lingkaran(3)
+
+print(f"Luas Persegi: {persegi.hitungLuas()}")
+print(f"Luas Lingkaran: {lingkaran.hitungLuas()}")
